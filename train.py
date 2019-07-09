295e42b21e5b1815b85784b8ef31e21cda5155c0
ZiadAbdElrahman <iziad2000@outlook.com>
2019-07-09 13:22:17 +0200

    .

--- train.py
+++ train.py
@@ -10,14 +10,12 @@ from util import save_weights, idx_to_sentence
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
 Data = DataLoader()
-Train_captions, Train_images, Train_feature, Train_lengths, img = Data.get_Training_data(400000)
-Test_captions, Test_images, Test_feature, Test_lengths = Data.get_val_data(150000)
+Train_captions, Train_images, Train_feature, Train_lengths, img = Data.get_Training_data()
+Test_captions, Test_images, Test_feature, Test_lengths = Data.get_val_data()
 
 
 def main(args):
-
     encoder = Encoder(args.embed_size).to(device)
     decoder = Decoder(args.embed_size, args.hidden_size, len(Data.word_to_idx), args.num_layers).to(device)
 
@@ -27,50 +25,48 @@ def main(args):
     optimizer = torch.optim.Adam(params, lr=args.learning_rate, weight_decay=args.rg)
 
     for epoch in range(args.num_epochs):
-
         avgTrainLoss = train_step(encoder, decoder, criterion, optimizer)
         avgTestLoss = val_step(encoder, decoder, criterion)
 
         print('Epoch [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}, TestLoss: {:.4f}, TestPerplexity: {:5.4f}'
-              .format(epoch, args.num_epochs, avgTrainLoss,
+              .format(epoch + 1, args.num_epochs, avgTrainLoss,
                       np.exp(avgTrainLoss), avgTestLoss, np.exp(avgTestLoss)))
 
+    save_weights(encoder, args.model_path + "encoder")
 
-    save_weights(encoder, args.model_path+"encoder")
-    save_weights(decoder, args.model_path+"decoder")
+    save_weights(decoder, args.model_path + "decoder")
 
     sample = Sample(encoder, decoder, device)
 
-    Train_ouutput = sample.predict(Train_feature[0:15])
-    Test_ouutput = sample.predict(Test_feature[0:15])
+    train_ouutput = sample.predict(Train_feature[Train_images[0:15]])
+    test_ouutput = sample.predict(Test_feature[Test_images[0:15]])
 
-    Train_sent = idx_to_sentence(Train_ouutput, Data.idx_to_word)
-    Train_GT = idx_to_sentence(Train_captions[0:15], Data.idx_to_word)
+    train_sent = idx_to_sentence(train_ouutput, Data.idx_to_word)
+    train_GT = idx_to_sentence(Train_captions[0:15], Data.idx_to_word)
 
-    Test_sent = idx_to_sentence(Test_ouutput, Data.idx_to_word)
-    Test_GT = idx_to_sentence(Test_captions[0:15], Data.idx_to_word)
-
-
-    for i in range(15):
-        print(Train_sent[i])
-        print(Train_GT[i])
-        print("")
-        print("")
-
-    print("")
-    print("")
-    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    print("")
-    print("")
-
+    test_sent = idx_to_sentence(test_ouutput, Data.idx_to_word)
+    test_GT = idx_to_sentence(Test_captions[0:15], Data.idx_to_word)
 
     for i in range(15):
-        print(Test_sent[i])
-        print(Test_GT[i])
+        print(train_sent[i])
+        print(train_GT[i])
         print("")
         print("")
+
+    print("")
+    print("")
+    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    print("")
+    print("")
+
+    for i in range(15):
+        print(test_sent[i])
+        print(test_GT[i])
+        print("")
+        print("")
+
 
 def train_step(encoder, decoder, criterion, optimizer):
     Train_totalLoss = 0
@@ -141,24 +137,18 @@ def val_step(encoder, decoder, criterion):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_path', type=str, default='models/', help='path for saving trained models')
-    parser.add_argument('--crop_size', type=int, default=224, help='size for randomly cropping images')
-    parser.add_argument('--vocab_path', type=str, default='data/vocab.pkl', help='path for vocabulary wrapper')
-    parser.add_argument('--image_dir', type=str, default='data/resized2014', help='directory for resized images')
-    parser.add_argument('--caption_path', type=str, default='data/annotations/captions_train2014.json',
-                        help='path for train annotation json file')
-    parser.add_argument('--log_step', type=int, default=10, help='step size for prining log info')
-    parser.add_argument('--save_step', type=int, default=1000, help='step size for saving trained models')
 
     # Model parameters
-    parser.add_argument('--embed_size', type=int, default=512, help='dimension of word embedding vectors')
-    parser.add_argument('--hidden_size', type=int, default=512, help='dimension of lstm hidden states')
+    parser.add_argument('--embed_size', type=int, default=128, help='dimension of word embedding vectors')
+    parser.add_argument('--hidden_size', type=int, default=128, help='dimension of lstm hidden states')
     parser.add_argument('--num_layers', type=int, default=2, help='number of layers in lstm')
 
-    parser.add_argument('--num_epochs', type=int, default=100)
-    parser.add_argument('--batch_size', type=int, default=50)
+    parser.add_argument('--num_epochs', type=int, default=300)
+    parser.add_argument('--batch_size', type=int, default=5)
     parser.add_argument('--num_workers', type=int, default=2)
-    parser.add_argument('--learning_rate', type=float, default=5e-4)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)
     parser.add_argument('--rg', type=float, default=0)
     args = parser.parse_args()
+
     print(args)
     main(args)
