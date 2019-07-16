1aa3ddaf95c89a58a23281d547948af74d261979
ZiadAbdElrahman <iziad2000@outlook.com>
2019-07-16 13:54:33 +0200

    improve the model by adding the feature to the h in every step

--- model.py
+++ model.py
@@ -2,14 +2,14 @@ import torch
 import torch.nn as nn
 
 
-
 class Encoder(nn.Module):
-    def __init__(self, input_size, embed_size):
+    def __init__(self, input_size, hidden_size):
         super(Encoder, self).__init__()
-        self.linear1 = nn.Linear(input_size, embed_size)
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.bn1 = nn.BatchNorm1d(hidden_size)
 
     def forward(self, image):
-        feature = (self.linear1(image))
+        feature = self.bn1(self.linear1(image))
         return feature
 
 
@@ -18,37 +18,36 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         self.embed = nn.Embedding(1004, embed_size)
         self.lstm = nn.LSTMCell(embed_size, hidden_size)
-        self.linear1 = nn.Linear(hidden_size, vocab_size)
+        self.linear = nn.Linear(hidden_size, vocab_size)
         self.max_seg_length = max_seq_length
 
     def forward(self, feature, captions, lengths):
-
         outputs = []
-        embeddings = self.embed(captions)
         h = feature
         c = torch.zeros_like(feature)
+        embeddings = self.embed(captions)
+        num_of_wards = embeddings.shape[1] - 1
 
-        for i in range(embeddings.shape[1]-1):
+        for i in range(num_of_wards):
             h, c = self.lstm(embeddings[:, i, :], (h, c))
             outputs.append(h)
-
+        h += feature
         outputs = torch.stack(outputs, 1)
-        outputs = self.linear1(outputs)
+        outputs = self.linear(outputs)
         return outputs
 
     def sample(self, feature, inputs):
         sampled_ids = []
-
         h = feature
         c = torch.zeros_like(feature)
-
         inputs = self.embed(inputs)
-        for i in range(16):
+
+        for i in range(self.max_seg_length):
             h, c = self.lstm(inputs, (h, c))
-            outputs = self.linear1(h)
+            outputs = self.linear(h)
             _, predicted = outputs.max(1)
             sampled_ids.append(predicted)
             inputs = self.embed(predicted)
-
+            h += feature
         sampled_ids = torch.stack(sampled_ids, 1)
         return sampled_ids
